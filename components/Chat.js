c2f9363ddf5bd9f31e42d9fd9952b32c426c0b6b
Sean Tansey <sean.d.tansey@gmail.com>
2019-01-31 14:53:04 -0700

    added chat

---
+++ components/Chat.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import {StyleSheet, Text, View} from 'react-native';
+
+
+export default class Chat extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+  
+  render() {
+    return (
+      <View>
+        <Text>work... pls.</Text>
+      </View>
+    )
+  }
+}
