2a5936851cf95dcd065fa3d0d71e6088614941a4
Ben Strater-Vidal <ben.strater-vidal@gmail.com>
2019-02-04 14:51:39 -0700

    Commited

--- components/Router.js
+++ components/Router.js
@@ -1,27 +1,15 @@
 import React from 'react'
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation'
 import Chat from './Chat/Chat'
-import Chatdisplay from './Chat/Chatdisplay'
 import SignUp from './SignUp/SignUp'
 import Home from './Profile/Home'
 import Schedule from './Schedule/Schedule'
-import ScheduleItem from './Schedule/ScheduleItem'
-
 const TabNavigator = createBottomTabNavigator({
-  Chat: Chatdisplay,
+  Chat: Chat,
   Profile: Home,
   SignUp: SignUp,
   Schedule: Schedule,
-  ScheduleItem: ScheduleItem
-}, {tabBarOptions: {
-  activeTintColor: '#7ed957',
-  labelStyle: {
-    fontSize: 14,
-  },
-  style: {
-    backgroundColor: '#545454',
-  },
-}})
+})
 
 
 export const Nav = createAppContainer(TabNavigator)
